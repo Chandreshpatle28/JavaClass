d54a8ad9d2b386d82359adac02f21a8713278ad0
Chandreshpatle28 <Chandreshpatle28>
2024-10-14 08:02:31 +0530

    Added single Inhertance Commands on OCTOBER 14

---
+++ src/oct14/Class.java
@@ -0,0 +1,18 @@
+package oct14;
+
+public class Class extends School {
+	
+	public void classSection()
+	{
+		System.out.println("This is the Class Section");
+	}
+
+	public static void main(String[] args) {
+		
+		Class sc=new Class();
+		sc.Schoolsection();
+		sc.classSection();
+
+	}
+
+}
---
+++ src/oct14/School.java
@@ -0,0 +1,10 @@
+package oct14;
+
+public class School {
+	
+	public void Schoolsection()
+	{
+		System.out.println("This is the School Section");
+	}
+
+}
